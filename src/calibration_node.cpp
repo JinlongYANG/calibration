1c30c3abb21dd8f08df8c830e0adf3d4e83e9fc0
Jinlong <jinlong@jinlong-ThundeRobot.(none)>
2014-05-09 13:52:22 +0200

    almost done the registration

--- src/calibration_node.cpp
+++ src/calibration_node.cpp
@@ -1,7 +1,9 @@
 #define BOOST_SIGNALS_NO_DEPRECATION_WARNING
 #include "calibration/calibration_node.hpp"
 #include "calibration/handkp_leap_msg_calibration.h"
+#include "calibration/handkp_leap_msg.h"
 #include "calibration/Ransac.h"
+#include "calibration/Hand_XYZRGB.h"
 #include <pcl_ros/point_cloud.h>
 #include <pcl/point_types.h>
 #include <math.h>
@@ -87,8 +89,11 @@ void Calibration_Node::syncedCallback(const ImageConstPtr& cvpointer_rgbImage,co
 
     pcl::PointCloud<pcl::PointXYZRGB> msg_pcl;
     pcl::PointCloud<pcl::PointXYZRGB> tooltipcloud;
+    pcl::PointCloud<pcl::PointXYZRGB> handcloud, hand1_kpt, hand2_kpt;
 
     ToolPosition tool_position;
+    HandKeyPoints hand_kpt;
+    Hand_XYZRGB hand1_XYZRGB, hand2_XYZRGB;
 
     ROS_INFO("Callback begins");
 
@@ -252,6 +257,131 @@ void Calibration_Node::syncedCallback(const ImageConstPtr& cvpointer_rgbImage,co
             cout << eigenTransform_visual2leap <<endl;
             cout<<"Estimate transform from leap to visual: "<< endl;
             cout << eigenTransform_leap2visual <<endl;
+
+            int seq = cvpointer_rgbInfo->header.seq;
+
+            /*******************   get color image and depth image    *******************/
+            cvpointer_rgbFrame = cv_bridge::toCvCopy(cvpointer_rgbImage);
+            cvpointer_depthFrame = cv_bridge::toCvCopy(cvpointer_depthImage);
+
+            ROS_INFO("current image seq: %d ",cvpointer_rgbInfo->header.seq);
+            BGRImage=cvpointer_rgbFrame->image;
+            cvtColor( BGRImage, BGRImage, CV_RGB2BGR);
+            DepthMat=cvpointer_depthFrame->image;
+            /*maximum depth: 3m*/
+            DepthImage = DepthMat*0.33;
+
+            /*******************   read in the leapmotion data   *******************/
+            hand_kpt.set_Leap_Msg(ptr_leap);
+            if(hand_kpt.hands_count != 0){
+                std::cout<< hand_kpt.hands_count<<std::endl;
+                hand1_XYZRGB.palm_center.x = hand_kpt.hand_position.at(0).x/1000.0;
+                hand1_XYZRGB.palm_center.y = hand_kpt.hand_position.at(0).y/1000.0;
+                hand1_XYZRGB.palm_center.z = hand_kpt.hand_position.at(0).z/1000.0;
+                uint8_t r1 = 0, g1 = 255, b1 = 0;
+                uint32_t rgb1 = ((uint32_t)r1 << 16 | (uint32_t)g1 << 8 | (uint32_t)b1);
+                hand1_XYZRGB.palm_center.rgb = *reinterpret_cast<float*>(&rgb1);
+                std::cout<<"hand1: palm center: "<<hand1_XYZRGB.palm_center.x<<" "<<hand1_XYZRGB.palm_center.y << " " << hand1_XYZRGB.palm_center.z<<std::endl;
+
+                pcl::PointXYZRGB h1_fingertips;
+                for(size_t i = 0; i < hand_kpt.fingertip_position.size ()/* && i < 5*/; ++i){
+                    h1_fingertips.x = hand_kpt.fingertip_position.at(i).x/1000.0;
+                    h1_fingertips.y = hand_kpt.fingertip_position.at(i).y/1000.0;
+                    h1_fingertips.z = hand_kpt.fingertip_position.at(i).z/1000.0;
+                    uint8_t rf = 0, gf = 150, bf = 150;
+                    uint32_t rgbf = ((uint32_t)rf << 16 | (uint32_t)gf << 8 | (uint32_t)bf);
+                    h1_fingertips.rgb = *reinterpret_cast<float*>(&rgbf);
+                    hand1_XYZRGB.fingertip_position.push_back(h1_fingertips);
+
+                }
+
+                if(hand_kpt.hands_count > 1){
+                    hand2_XYZRGB.palm_center.x = hand_kpt.hand_position.at(1).x/1000.0;
+                    hand2_XYZRGB.palm_center.y = hand_kpt.hand_position.at(1).y/1000.0;
+                    hand2_XYZRGB.palm_center.z = hand_kpt.hand_position.at(1).z/1000.0;
+                    uint8_t r2 = 0, g2 = 255, b2 = 0;
+                    uint32_t rgb2 = ((uint32_t)r2 << 16 | (uint32_t)g2 << 8 | (uint32_t)b2);
+                    hand2_XYZRGB.palm_center.rgb = *reinterpret_cast<float*>(&rgb2);
+                    std::cout<<"hand2: palm center: "<<hand2_XYZRGB.palm_center.x<<" "<<hand2_XYZRGB.palm_center.y << " " << hand2_XYZRGB.palm_center.z<<std::endl;
+                }
+            }
+
+            /*******************   Leap motion to Xtion coordinate transform   *******************/
+
+            if(hand_kpt.hands_count != 0){
+                PointXYZRGB after_transform;
+                Mat transform;
+                transform = Mat::zeros(4,4, CV_32F);
+                eigen2cv(eigenTransform_leap2visual, transform);
+                after_transform.x = hand1_XYZRGB.palm_center.x*transform.at<float>(0,0)+hand1_XYZRGB.palm_center.y*transform.at<float>(0,1)+hand1_XYZRGB.palm_center.z*transform.at<float>(0,2)+transform.at<float>(0,3);
+                after_transform.y = hand1_XYZRGB.palm_center.x*transform.at<float>(1,0)+hand1_XYZRGB.palm_center.y*transform.at<float>(1,1)+hand1_XYZRGB.palm_center.z*transform.at<float>(1,2)+transform.at<float>(1,3);
+                after_transform.z = hand1_XYZRGB.palm_center.x*transform.at<float>(2,0)+hand1_XYZRGB.palm_center.y*transform.at<float>(2,1)+hand1_XYZRGB.palm_center.z*transform.at<float>(2,2)+transform.at<float>(2,3);
+                hand1_kpt.push_back(after_transform);
+
+                std::cout<<"Palm center: "<<hand1_kpt.at(0).x<<" "<<hand1_kpt.at(0).y << " " << hand1_kpt.at(0).z<<std::endl;
+                for(size_t i = 0; i < hand1_XYZRGB.fingertip_position.size(); ++i){
+                    PointXYZRGB finger_after_transform;
+                    finger_after_transform.x = hand1_XYZRGB.fingertip_position.at(i).x*transform.at<float>(0,0)+hand1_XYZRGB.fingertip_position.at(i).y*transform.at<float>(0,1)+hand1_XYZRGB.fingertip_position.at(i).z*transform.at<float>(0,2)+transform.at<float>(0,3);
+                    finger_after_transform.y = hand1_XYZRGB.fingertip_position.at(i).x*transform.at<float>(1,0)+hand1_XYZRGB.fingertip_position.at(i).y*transform.at<float>(1,1)+hand1_XYZRGB.fingertip_position.at(i).z*transform.at<float>(1,2)+transform.at<float>(1,3);
+                    finger_after_transform.z = hand1_XYZRGB.fingertip_position.at(i).x*transform.at<float>(2,0)+hand1_XYZRGB.fingertip_position.at(i).y*transform.at<float>(2,1)+hand1_XYZRGB.fingertip_position.at(i).z*transform.at<float>(2,2)+transform.at<float>(2,3);
+                    finger_after_transform.rgb = hand1_XYZRGB.fingertip_position.at(i).rgb;
+                    hand1_kpt.push_back(finger_after_transform);
+                    std::cout<<"finger "<<i<<": "<<hand1_kpt.at(i).x<<" "<<hand1_kpt.at(i).y << " " << hand1_kpt.at(i).z<<std::endl;
+                }
+                std::cout<<"Size: "<<hand1_kpt.size()<<std::endl;
+
+                /*******************   get point cloud    *******************/
+                fromROSMsg(*pclpointer_pointCloud2, msg_pcl);
+
+                std::cout<<msg_pcl.points.size()<<std::endl;}
+
+            /*******************   segment hand from the point cloud   *******************/
+            pcl::PointXYZRGB p;
+            uint8_t r = 255, g = r, b = r;
+            uint32_t rgb = ((uint32_t)r << 16 | (uint32_t)g << 8 | (uint32_t)b);
+
+            for (size_t i = 0; i < msg_pcl.points.size (); ++i){
+                if(msg_pcl.points[i].z < max_depth && msg_pcl.points[i].z > min_depth){
+                    p.rgb = *reinterpret_cast<float*>(&rgb);
+                    p.x = msg_pcl.points[i].x;
+                    p.y = msg_pcl.points[i].y;
+                    p.z = msg_pcl.points[i].z;
+                    handcloud.push_back(p);
+                }
+            }
+            /*******************   Convert the CvImage to a ROS image message and publish it to topics.   *******************/
+            cv_bridge::CvImage bgrImage_msg;
+            bgrImage_msg.encoding = sensor_msgs::image_encodings::BGR8;
+            bgrImage_msg.image    = BGRImage;
+            bgrImage_msg.header.seq = seq;
+            bgrImage_msg.header.frame_id = seq;
+            bgrImage_msg.header.stamp = ros::Time::now();
+            bgrImagePublisher_.publish(bgrImage_msg.toImageMsg());
+
+            cv_bridge::CvImage depthImage_msg;
+            depthImage_msg.encoding = sensor_msgs::image_encodings::TYPE_32FC1;
+            depthImage_msg.image    = DepthImage;
+            depthImage_msg.header.seq = seq;
+            depthImage_msg.header.frame_id = seq;
+            depthImage_msg.header.stamp = ros::Time::now();
+            depthImagePublisher_.publish(depthImage_msg.toImageMsg());
+
+            /*******************   publish pointCloud   *******************/
+            sensor_msgs::PointCloud2 cloud_msg;
+            toROSMsg(handcloud,cloud_msg);
+            cloud_msg.header.frame_id=cvpointer_depthInfo->header.frame_id;
+            cloud_pub_.publish(cloud_msg);
+
+            sensor_msgs::PointCloud2 hand1_kpt_msg;
+            toROSMsg(hand1_kpt,hand1_kpt_msg);
+            hand1_kpt_msg.header.frame_id=cvpointer_depthInfo->header.frame_id;
+            hkp_cloud_pub_.publish(hand1_kpt_msg);
+
+            /*******************   clear data   *******************/
+            hand_kpt.Clear();
+
+
+            ROS_INFO("One callback done");
         }
     }
     catch (std::exception& e)
